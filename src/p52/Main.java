1db997e52d58ae13af0539121e1f65436cbb215d
andreas <andreas@ubuntu-7B79>
2025-06-09 18:34:30 +0200

    Praesenz 5 - A2 - Theorie

--- src/p52/Main.java
+++ src/p52/Main.java
@@ -1,7 +1,6 @@
 package p52;
 
 public class Main {
-
 	public static void main(String[] args) {
 		CheckoutService paypalService = new CheckoutService(new PayPalPayment());
 		paypalService.processPayment(50.0);
@@ -10,3 +9,44 @@ public class Main {
 	}
 
 }
+/*
+ * 1) Which SOLID principles can you identify in your design ?
+ * 
+ * Single Responsibility Principle (SRP): Each class has one responsibility:
+ * PayPalPayment and CreditCardPayment handle payments, CheckoutService
+ * processes them.
+ * 
+ * Open-Closed Principle (OCP): The system can be extended with new payment
+ * providers without modifying existing classes.
+ * 
+ * Liskov Substitution Principle (LSP): Any class implementing PaymentProvider
+ * can be used by CheckoutService without affecting its correctness.
+ * 
+ * Dependency Inversion Principle (DIP): CheckoutService depends on the
+ * PaymentProvider abstraction, not on specific implementations
+ * 
+ * 2) How does your code fulfill the Open-Closed Principle?
+ * 
+ * Open-Closed Principle: New payment methods can be added by creating new
+ * classes that implement PaymentProvider. CheckoutService does not need to
+ * change, which means the system is open for extension, closed for
+ * modification.
+ * 
+ * 3) Why can CheckoutService work with any PaymentProvider implementation? (->
+ * Liskov Substitution Principle)
+ * 
+ * Subclasses (or implementations) can be used wherever the superclass (or
+ * interface) is expected. CheckoutService only relies on the PaymentProvider
+ * interface. Any class implementing this interface can be passed in and used
+ * without breaking or altering the logic.
+ * 
+ * 4) Your colleague suggests extending the PaymentProvider interface with
+ * additional methods such as void validateCard(); and void checkCreditScore();
+ * in order to bundle all payment types into a single interface. Would this
+ * violate any of the SOLID principles?
+ * 
+ * Adding these methods to the interface PaymentProvider violate the Interface
+ * Segregation Principle. Because some providers (like PayPal) may not need
+ * methods like validateCard() or checkCreditScore(). They would be forced to
+ * implement methods they don't use (Violation).
+ */
