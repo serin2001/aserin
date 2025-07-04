cfa19f08462f5996b763ade26c5ac13d30b923d1
andreas <andreas@ubuntu-7B79>
2025-07-04 11:00:44 +0200

    Blatt 9 - A3

---
+++ src/ex93/MovieStreamDemo.java
@@ -0,0 +1,43 @@
+package ex93;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class MovieStreamDemo {
+	static class Movie {
+		String title;
+		double rating;
+		int year;
+
+		Movie(String title, double rating, int year) {
+			this.title = title;
+			this.rating = rating;
+			this.year = year;
+		}
+
+		public String getTitle() {
+			return title;
+		}
+
+		public double getRating() {
+			return rating;
+		}
+
+		public int getYear() {
+			return year;
+		}
+
+		@Override
+		public String toString() {
+			return title + " (" + year + ") - Rating: " + rating;
+		}
+	}
+
+	public static void main(String args[]) {
+		List<Movie> movies = List.of(new Movie("Inception", 8.8, 2010), new Movie("The Godfather", 9.2, 1972),
+				new Movie("The Room", 3.7, 2003), new Movie("Interstellar", 8.6, 2014), new Movie("Joker", 8.4, 2019));
+
+		movies.stream().filter(m -> m.getRating() >= 8.5).sorted(Comparator.comparing(Movie::getYear)
+				.thenComparing(Comparator.comparing(Movie::getRating))).forEach(System.out::println);
+	}
+}
