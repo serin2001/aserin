e4c0fe992657aeace5d765aa5477800b293c7fee
andreas <andreas@ubuntu-7B79>
2025-07-18 13:50:40 +0200

    Blatt 3 - A1 - Access pin directly in CompanyAccount

--- src/ex31diff/CompanyAccount.java
+++ src/ex31diff/CompanyAccount.java
@@ -8,19 +8,18 @@ public class CompanyAccount extends Account {
 		super(owner, balance, pin);
 	}
 
-	public void accessAccount(Account account) {
-		System.out.println(account.owner);
+	public void accessAccount(CompanyAccount account) {
 		System.out.println(account.owner);
 		System.out.println(account.getBalance());
-		// System.out.println(account.pin);
+		System.out.println(account.pin);
 		// System.out.println(account.internalNote);
 		/*
 		 * Explain which accesses fail and why.
 		 * Exception in thread "main" java.lang.Error: Unresolved compilation problems:
-		 * The field Account.pin is not visible
 		 * The field Account.internalNote is not visible
 		 * see slide 17 of lecture 2
 		 * What is the difference between using protected vs. using private and providing a getter for subclasses to access an instance variable?
+		 * Trying to access pin through an instance of Account will cause a compilation error. Access pin directly in CompanyAccount, but not through an Account reference.
 		 * see slide 17 of lecture 2
 		 */
 		// Suggest alternative designs (e.g., getters).
