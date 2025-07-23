3091738459628cde0fbe98743e848a923c7afda4
andreas <andreas@ubuntu-7B79>
2025-07-23 11:59:53 +0200

    Blatt 9 - A3 - Sortierung (Demonstartion)

--- src/ex93/MovieStreamDemo.java
+++ src/ex93/MovieStreamDemo.java
@@ -34,10 +34,20 @@ public class MovieStreamDemo {
 	}
 
 	public static void main(String args[]) {
-		List<Movie> movies = List.of(new Movie("Inception", 8.8, 2010), new Movie("The Godfather", 9.2, 1972),
-				new Movie("The Room", 3.7, 2003), new Movie("Interstellar", 8.6, 2014), new Movie("Joker", 8.4, 2019));
+        List<Movie> movies = List.of(
+                new Movie("Inception", 8.8, 2010),
+                new Movie("The Godfather", 9.2, 1972),
+                new Movie("The Room", 3.7, 2003),
+                new Movie("Interstellar", 8.6, 2014),
+                new Movie("Joker", 8.4, 2019),
+                new Movie("Guardians of the Galaxy", 8.0, 2014), // New movie
+                new Movie("The Lego Movie", 7.8, 2014) // New movie
+            );
 
-		movies.stream().filter(m -> m.getRating() >= 8.5).sorted(Comparator.comparing(Movie::getYear)
-				.thenComparing(Comparator.comparing(Movie::getRating))).forEach(System.out::println);
+		movies.stream()
+			.filter(m -> m.getRating() >= 7.5)
+			.sorted(Comparator.comparing(Movie::getYear)
+			.thenComparing(Comparator.comparing(Movie::getRating).reversed()))
+			.forEach(System.out::println);
 	}
 }
