29b9e47890fb272133f2a39658bffcedfa3f7837
andreas <andreas@ubuntu-7B79>
2025-07-29 21:48:21 +0200

    Praesenz 6 - A2 - try enthaelt Erstellung beider Threads

--- src/p62/BankAccountSimulator.java
+++ src/p62/BankAccountSimulator.java
@@ -1,17 +1,15 @@
 package p62;
 
 public class BankAccountSimulator {
-	
+
 	public static void main(String[] args) {
 		BankAccount account = new BankAccount(1000);
-		
-		Thread t1 = new TransactionThread(account);
-		Thread t2 = new TransactionThread(account);
-		
-		t1.start();
-		t2.start();
-		
 		try {
+			Thread t1 = new TransactionThread(account);
+			Thread t2 = new TransactionThread(account);
+
+			t1.start();
+			t2.start();
 			t1.join();
 			t2.join();
 		} catch (InterruptedException e) {
